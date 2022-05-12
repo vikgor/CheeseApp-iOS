2093cb73bc1d474ff03f1ad6de4a91c604784be3
v-gordienko <v.gordienko@indriver.com>
2022-05-12 12:52:02 +0600

    Initial Commit

---
+++ CheeseApp/CheeseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheeseAppApp.swift
+//  CheeseApp
+//
+//  Created by Viktor Gordienko on 12.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct CheeseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CheeseApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CheeseApp
+//
+//  Created by Viktor Gordienko on 12.05.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
